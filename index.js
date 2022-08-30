336b0c4a18800e5bc09df0baa0372488e32a8d47
Michael <mikahado@gmail.com>
2022-08-30 00:12:29 -0400

    control flow completed

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if (num <= 400) {
+    return 'This one is on me!'
+  } else if (num > 400 && num <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (num > 2000 && num < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (num >= 2500 )
+    return 'No can do.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  const location = city
+  let cityMsg
+  cityMsg = location === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMsg;
+    }
+
+
+function switchOnCharmFromTip(tip){
+
+  let msg;
+
+    switch (tip) {
+      case 'generous':
+        msg = 'Thank you so much.';
+        break;
+      case 'not as generous':
+        msg = 'Thank you.';
+        break;
+      default: 
+        msg = 'Bye.'
+        break;
+    }
+    return msg
+  }
